d6b18b5f31d238856aa425851ce48d71024523da
Steveen <steveen.echeverri@gmail.com>
2020-01-25 18:16:54 -0500

    Basic File react

---
+++ src/components/Facebook.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
+
+export default class Facebook extends Component {
+
+    state = {
+        isLoggedIn : false,
+        userId : '',
+        name : '',
+        email : '',
+        picture : ''
+    }
+
+    render() {
+        
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
